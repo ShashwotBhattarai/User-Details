1487dd07de37e54a3e2736996ca2c4c39a871975
Shashwot Bhattarai <shashwot.web@gmail.com>
2020-11-02 21:58:59 +0545

    PR review request change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { UrlEnum } from './common/enum/routing.enum';
 import { MainComponent } from './Main/Main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
