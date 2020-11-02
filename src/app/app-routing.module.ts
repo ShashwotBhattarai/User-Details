100935bda0c405ed298bc2695f398496e7a65654
Shashwot Bhattarai <shashwot.web@gmail.com>
2020-11-02 21:48:09 +0545

    Url replaced with enum in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { RegistrationComponent } from './registration/registration.component';
 
+import { UrlEnum } from './common/enum/routing.enum';
 import { MainComponent } from './Main/Main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'app-main', component: MainComponent },
-  { path: 'app-registration', component: RegistrationComponent },
+  { path: UrlEnum.MAIN, component: MainComponent },
+  { path: UrlEnum.REGISTRATION, component: RegistrationComponent },
 ];
 
 @NgModule({
